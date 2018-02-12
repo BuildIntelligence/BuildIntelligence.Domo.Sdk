841ebfe231803c6e5d284728a07d4e88e265a748
Ryan Wilson <ryan.wilson@buildintelligence.com>
2018-02-12 10:22:24 -0800

    added doc strings to top level interfaces

--- BiDomoDotNet/IDomoConfig.cs
+++ BiDomoDotNet/IDomoConfig.cs
@@ -4,10 +4,29 @@ namespace BiDomoDotNet
 {
     public interface IDomoConfig
 	{
+        /// <summary>
+        /// Domo App Client Id for Auth
+        /// </summary>
 		string ClientId { get; set; }
+
+        /// <summary>
+        /// Domo App Secret for Auth
+        /// </summary>
 		string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Base URI for api
+        /// </summary>
 		Uri ApiHost { get; set; }
+
+        /// <summary>
+        /// Scope for Authorization. Data and/or Users scopes
+        /// </summary>
 		DomoAuthScope Scope { get; set; }
+
+        /// <summary>
+        /// CLI Logging Verbosity for Http Requests
+        /// </summary>
 		HttpLoggingLevel HttpLogging { get; set; }
 	}
 }
--- BiDomoDotNet/IGotDomod.cs
+++ BiDomoDotNet/IGotDomod.cs
@@ -8,10 +8,29 @@ namespace BiDomoDotNet
 {
     public interface IGotDomod
     {
+        /// <summary>
+        /// Property for interacting with Datasets API
+        /// </summary>
         IDomoDatasetClient Datasets { get; set; }
+
+        /// <summary>
+        /// Property for interacting with Groups API
+        /// </summary>
         IDomoGroupClient Groups { get; set; }
+
+        /// <summary>
+        /// Property for interacting with Pages API
+        /// </summary>
         IDomoPageClient Pages { get; set; }
+
+        /// <summary>
+        /// Property for interacting with Streams API
+        /// </summary>
         IDomoStreamClient Streams { get; set; }
+
+        /// <summary>
+        /// Property for interacting with Users API
+        /// </summary>
         IDomoUserClient Users { get; set; }
     }
 }
